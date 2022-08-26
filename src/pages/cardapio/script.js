eb3724fa5ea3e5e7bf3e4466c2f7653e46fc4fe4
Nika Rosa <nigorosa@gmail.com>
2022-08-26 17:53:01 -0300

    segFinal

--- src/pages/cardapio/script.js
+++ src/pages/cardapio/script.js
@@ -1,6 +1,8 @@
+var carrinho = document.querySelector('[data-carrinho]')
 var quantidade = document.querySelector('[data-qtd]')
 var check = document.getElementsByName('produto')
 var modalFinale = document.querySelector('[data-finale]')
+var cartValue = document.querySelector('[data-cartValue]')
 
 var resume = document.querySelector('[data-resume]')
 var misto = document.querySelector('[data-misto]')
@@ -14,79 +16,153 @@ var limao = document.querySelector('[data-sucoLimao]')
 var refri = document.querySelector('[data-refri]')
 var heineken = document.querySelector('[data-heineken]')
 var pedidos = document.querySelector('[data-tabelaPedidos]')
-var valor = 0
+var fecharModal = document.querySelector('.close')
+var ingredientes = document.querySelectorAll('[data-ingrediente]')
+var largura = window.innerWidth
 
+var total = 0
+var valor = 0
+var esseProduto
+
+//MOSTRAR MODAL INSERIR NO CARRINHO
+function showFinale(){
+    modalFinale.classList.remove('hide')
+    modalFinale.classList.add('finale')
+}
+
+//PRODUTOS
+
+//TRATAR INGREDIENTES
+if(largura <= 800){
+    ingredientes.forEach((elemento) => {elemento.classList.remove("ingredientes")})
+    ingredientes.forEach((elemento) => {elemento.classList.add("hide")}) 
+}else if(largura > 800){
+    ingredientes.forEach((elemento) => {elemento.classList.remove("hide")}) 
+    ingredientes.forEach((elemento) => {elemento.classList.add("ingredientes")})
+}
+
+//SELECIONAR PRODUTO
 misto.addEventListener("click", () => {
+    showFinale()
     valor = 4
     if(misto.checked == true){
-        var esseProduto = "Misto Quente"
+        esseProduto = "Misto Quente"
         resume.innerHTML = `<p>${esseProduto} | R$ ${valor.toFixed(2)}`
     }
 })
 bomba.addEventListener("click", () => {
+    showFinale()
     valor = 8
     if(bomba.checked == true){
-        var esseProduto = "X-Bomba"
+        esseProduto = "X-Bomba"
         resume.innerHTML = `<p>${esseProduto} | R$ ${valor.toFixed(2)}`
     }
 })
 tudo.addEventListener("click", () => {
+    showFinale()
     valor = 17
     if(tudo.checked == true){
-        var esseProduto = "X-Tudo"
+        esseProduto = "X-Tudo"
         resume.innerHTML = `<p>${esseProduto} | R$ ${valor.toFixed(2)}`
     }
 })
 frango.addEventListener("click", () => {
+    showFinale()
     valor = 19
     if(frango.checked == true){
-        var esseProduto = "X-Tudo de Frango"
+        esseProduto = "X-Tudo de Frango"
         resume.innerHTML = `<p>${esseProduto} | R$ ${valor.toFixed(2)}`
     }
 })
 brocado.addEventListener("click", () => {
+    showFinale()
     valor = 25
     if(brocado.checked == true){
-        var esseProduto = "X-Brocado"
+        esseProduto = "X-Brocado"
         resume.innerHTML = `<p>${esseProduto} | R$ ${valor.toFixed(2)}`
     }
 })
 mara.addEventListener("click", () => {
+    showFinale()
     valor = 6
     if(mara.checked == true){
-        var esseProduto = "Suco de Maracujá"
+        esseProduto = "Suco de Maracujá"
         resume.innerHTML = `<p>${esseProduto} | R$ ${valor.toFixed(2)}`
     }
 })
 laranja.addEventListener("click", () => {
+    showFinale()
     valor = 6
     if(laranja.checked == true){
-        var esseProduto = "Suco de Laranja"
+        esseProduto = "Suco de Laranja"
         resume.innerHTML = `<p>${esseProduto} | R$ ${valor.toFixed(2)}`
     }
 })
 limao.addEventListener("click", () => {
+    showFinale()
     valor = 6
     if(limao.checked == true){
-        var esseProduto = "Suco de Limão"
+        esseProduto = "Suco de Limão"
         resume.innerHTML = `<p>${esseProduto} | R$ ${valor.toFixed(2)}`
     }
 })
 refri.addEventListener("click", () => {
+    showFinale()
     valor = 5
     if(refri.checked == true){
-        var esseProduto = "Refrigerante Lata"
+        esseProduto = "Refrigerante Lata"
         resume.innerHTML = `<p>${esseProduto} | R$ ${valor.toFixed(2)}`
     }
 })
 heineken.addEventListener("click", () => {
+    showFinale()
     valor = 8
     if(heineken.checked == true){
-        var esseProduto = "Heineken Long Neck"
+        esseProduto = "Heineken Long Neck"
         resume.innerHTML = `<p>${esseProduto} | R$ ${valor.toFixed(2)}`
     }
 })
 
+// MODAL INSERIR NO CARRINHO
+
+//BOTAO QUANTIDADE
+function step(botao){
+    let id = botao.getAttribute("id")
+    let value = quantidade.getAttribute("value")
+    let step = quantidade.getAttribute("step")
+    let calcStep = (id == "dim") ? (step * -1) : (step * 1)
+    let newValue = parseInt(value) + calcStep
+    
+    quantidade.setAttribute("value", newValue)
+}
+
+//BOTAO INSERIR
+document.querySelector('[data-insert]').addEventListener('click', () => {
+    let essaQtd = quantidade.getAttribute('value')
+    let totalI = valor*essaQtd
+    cart(esseProduto, essaQtd, valor.toFixed(2), totalI.toFixed(2))
+
+    var numero = pedidos.rows.length
+    cartValue.innerText = (numero-1)
+
+    total = total + totalI
+
+    cartValue.classList.remove('hide')
+    cartValue.classList.add('showCartValue')
+    
+    document.querySelector('[data-message]').innerText = `R$ ${total.toFixed(2)}`
+    console.log(largura)
+})
+
+//BOTAO FECHAR MODAL
+fecharModal.addEventListener("click", () => {
+    modalFinale.classList.remove('finale')
+    modalFinale.classList.toggle('hide')
+})
+
+// CARRINHO
+
+//TABELA PEDIDOS
 function cart(produto, qtd, val, valt){
     
     var qtdLinhas = pedidos.rows.length
@@ -103,32 +179,20 @@ function cart(produto, qtd, val, valt){
     cellQtd.innerHTML = qtd
     cellVal.innerHTML = val
     cellValT.innerHTML = valt
+
 }
 
-check.forEach(addEventListener("click", () => {
-    modalFinale.classList.remove('hide')
-    modalFinale.classList.add('finale')
-}))
+//BOTAO ACESSAR CARRINHO
+document.querySelector('[data-cart]').addEventListener('click', () => {
+    carrinho.classList.remove('hide')
+    carrinho.classList.add('modalConfirm')
 
-
-function step(botao){
-    let id = botao.getAttribute("id")
-    let value = quantidade.getAttribute("value")
-    let step = quantidade.getAttribute("step")
-    let calcStep = (id == "dim") ? (step * -1) : (step * 1)
-    let newValue = parseInt(value) + calcStep
-    
-    quantidade.setAttribute("value", newValue)
-}
-
-document.querySelector('[data-button]').addEventListener("click", () =>{
-    let essaQtd = quantidade.getAttribute('value')
-    cart(esseProduto, essaQtd, valor.toFixed(2), (valor*essaQtd).toFixed(2))
-
-    quantidade.setAttribute("value", 0)
-})
-
-function close() {
     modalFinale.classList.remove('finale')
     modalFinale.classList.add('hide')
-}
+})
+
+//BOTAO SAIR DO CARRINHO
+document.querySelector('[data-back]').addEventListener('click', () => {
+    carrinho.classList.remove('modalConfirm')
+    carrinho.classList.add('hide')
+})
