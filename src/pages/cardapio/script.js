d0b6ec2734268108944235b3f1c45b2aabfe0804
Nika Rosa <nigorosa@gmail.com>
2022-08-25 22:03:12 -0300

    primeiroFinal

--- src/pages/cardapio/script.js
+++ src/pages/cardapio/script.js
@@ -1,132 +1,134 @@
+var quantidade = document.querySelector('[data-qtd]')
+var check = document.getElementsByName('produto')
+var modalFinale = document.querySelector('[data-finale]')
+
+var resume = document.querySelector('[data-resume]')
+var misto = document.querySelector('[data-misto]')
 var bomba = document.querySelector('[data-bomba]')
+var tudo = document.querySelector('[data-tudo]')
 var frango = document.querySelector('[data-frango]')
-var artesanal = document.querySelector('[data-artesanal]')
-var chapa = document.querySelector('[data-chapa]')
-var molho = document.querySelector('[data-molho]')
-var sucoMara = document.querySelector('[data-sucoMara]')
-var sucoCaja = document.querySelector('[data-sucoCaja')
+var brocado = document.querySelector('[data-brocado]')
+var mara = document.querySelector('[data-sucoMara]')
+var laranja = document.querySelector('[data-sucoLaranja]')
+var limao = document.querySelector('[data-sucoLimao]')
 var refri = document.querySelector('[data-refri]')
 var heineken = document.querySelector('[data-heineken]')
-
-var modal = document.querySelector('[data-modalConfirm]')
-var message = document.querySelector('[data-message]')
-var confirm = document.querySelector('[data-confirm]')
-
-var qtdBomba = document.querySelector('[data-qtdBomba]')
-var qtdFrango = document.querySelector('[data-qtdFrango]')
-var qtdArtesanal = document.querySelector('[data-qtdArtesanal]')
-var qtdChapa = document.querySelector('[data-qtdChapa]')
-var qtdMolho = document.querySelector('[data-qtdMolho]')
-var qtdMara = document.querySelector('[data-qtdMara]')
-var qtdCaja = document.querySelector('[data-qtdCaja]')
-var qtdRefri = document.querySelector('[data-qtdRefri]')
-var qtdHeineken = document.querySelector('[data-qtdHeineken]')
-
-var tabela = document.querySelector('[data-tabela]')
 var pedidos = document.querySelector('[data-tabelaPedidos]')
-var botao = document.querySelector('[data-button]')
+var valor = 0
 
-var qtd = document.getElementsByName("qtd")
-
-function confirmarValor(){
-    
-    function cart(produto, qtd, val, valt){
-        
-        var qtdLinhas = pedidos.rows.length
-        var linha = pedidos.insertRow(qtdLinhas)
-        
-        var cellCod = linha.insertCell(0)
-        var cellProduto = linha.insertCell(1)
-        var cellQtd = linha.insertCell(2)
-        var cellVal = linha.insertCell(3)
-        var cellValT = linha.insertCell(4)
-        
-        cellCod.innerHTML = qtdLinhas;
-        cellProduto.innerHTML = produto
-        cellQtd.innerHTML = qtd
-        cellVal.innerHTML = val
-        cellValT.innerHTML = valt
-    }
-    
-    var somaTotal = 0
-    if (bomba.checked == true) {
-        somaTotal = somaTotal + (9.99*qtdBomba.value)
-        cart("X-Bomba", qtdBomba.value, 9.99, (9.99*qtdBomba.value).toFixed(2))
-    }
-    if (frango.checked == true) {
-        somaTotal = somaTotal + (14.99*qtdFrango.value)
-        cart("X-Tudo de Frango", qtdFrango.value, 14.99, (14.99*qtdFrango.value).toFixed(2))
-    }
-    if(artesanal.checked == true){
-        somaTotal = somaTotal + (24.99*qtdArtesanal.value)
-        cart("Artesanal de Picanha", qtdArtesanal.value, 24.99, (qtdArtesanal.value*24.99).toFixed(2))
-    }
-    if (chapa.checked == true) {
-        somaTotal = somaTotal + (7.99*qtdChapa.value)
-        cart("Dog na Chapa", qtdChapa.value, 7.99, (7.99*qtdChapa.value).toFixed(2))
-    }
-    if(molho.checked == true){
-        somaTotal = somaTotal + (7.99*qtdMolho.value)
-        cart("Dog no Molho", qtdMolho.value, 7.99, (7.99*qtdMolho.value).toFixed(2))
-    }
-    if (sucoMara.checked == true) {
-        somaTotal = somaTotal + (5.99*qtdMara.value)
-        cart("Suco de Maracujá", qtdMara.value, 5.99, (5.99*qtdMara.value).toFixed(2))
-    }
-    if (sucoCaja.checked == true) {
-        somaTotal = somaTotal + (5.99*qtdCaja.value)
-        cart("Suco de Cajá", qtdCaja.value, 5.99, (5.99*qtdCaja.value).toFixed(2))
-    }
-    if (refri.checked == true) {
-        somaTotal = somaTotal + (4.99*qtdRefri.value)
-        cart("Refrigerante Lata", qtdRefri.value, 4.99, (4.99*qtdRefri.value).toFixed(2))
-    }
-    if (heineken.checked == true)  {
-        somaTotal = somaTotal + (8.99*qtdHeineken.value)
-        cart("Heineken Long Neck", qtdHeineken.value, 8.99, (8.99*qtdHeineken.value).toFixed(2))
-    }
-
-    modal.classList.remove("hide")
-    modal.classList.add("modalConfirm")
-
-    message.innerHTML = `R$ ${somaTotal}`
-}
-
-function voltar(){
-    somaTotal = 0
-
-    bomba.checked = false
-    frango.checked = false
-    artesanal.checked = false
-    chapa.checked = false
-    molho.checked = false
-    sucoMara.checked = false
-    sucoCaja.checked = false
-    refri.checked = false
-    heineken.checked = false
-
-    qtdArtesanal.value = ""
-    qtdBomba.value = ""
-    qtdCaja.value = ""
-    qtdChapa.value = ""
-    qtdMara.value = ""
-    qtdMolho.value = ""
-    qtdHeineken.value = ""
-    qtdFrango.value = ""
-    qtdRefri.value = ""
-    
-    modal.classList.remove("modalConfirm")
-    modal.classList.add("hide")
-
-    for (let i = 1; (pedidos.rows.length-1) > 1; i++){
-        pedidos.deleteRow(i)
-    }
-
-}
-
-document.addEventListener("keypress", function(e){
-
-    if(e.key === "Enter"){
-        botao.click()
+misto.addEventListener("click", () => {
+    valor = 4
+    if(misto.checked == true){
+        var esseProduto = "Misto Quente"
+        resume.innerHTML = `<p>${esseProduto} | R$ ${valor.toFixed(2)}`
     }
 })
+bomba.addEventListener("click", () => {
+    valor = 8
+    if(bomba.checked == true){
+        var esseProduto = "X-Bomba"
+        resume.innerHTML = `<p>${esseProduto} | R$ ${valor.toFixed(2)}`
+    }
+})
+tudo.addEventListener("click", () => {
+    valor = 17
+    if(tudo.checked == true){
+        var esseProduto = "X-Tudo"
+        resume.innerHTML = `<p>${esseProduto} | R$ ${valor.toFixed(2)}`
+    }
+})
+frango.addEventListener("click", () => {
+    valor = 19
+    if(misto.checked == true){
+        var esseProduto = "X-Tudo de Frango"
+        resume.innerHTML = `<p>${esseProduto} | R$ ${valor.toFixed(2)}`
+    }
+})
+brocado.addEventListener("click", () => {
+    valor = 25
+    if(brocado.checked == true){
+        var esseProduto = "X-Brocado"
+        resume.innerHTML = `<p>${esseProduto} | R$ ${valor.toFixed(2)}`
+    }
+})
+mara.addEventListener("click", () => {
+    valor = 6
+    if(mara.checked == true){
+        var esseProduto = "Suco de Maracujá"
+        resume.innerHTML = `<p>${esseProduto} | R$ ${valor.toFixed(2)}`
+    }
+})
+laranja.addEventListener("click", () => {
+    valor = 6
+    if(laranja.checked == true){
+        var esseProduto = "Suco de Laranja"
+        resume.innerHTML = `<p>${esseProduto} | R$ ${valor.toFixed(2)}`
+    }
+})
+limao.addEventListener("click", () => {
+    valor = 6
+    if(limao.checked == true){
+        var esseProduto = "Suco de Limão"
+        resume.innerHTML = `<p>${esseProduto} | R$ ${valor.toFixed(2)}`
+    }
+})
+refri.addEventListener("click", () => {
+    valor = 5
+    if(refri.checked == true){
+        var esseProduto = "Refrigerante Lata"
+        resume.innerHTML = `<p>${esseProduto} | R$ ${valor.toFixed(2)}`
+    }
+})
+heineken.addEventListener("click", () => {
+    valor = 8
+    if(heineken.checked == true){
+        var esseProduto = "Heineken Long Neck"
+        resume.innerHTML = `<p>${esseProduto} | R$ ${valor.toFixed(2)}`
+    }
+})
+
+function cart(produto, qtd, val, valt){
+    
+    var qtdLinhas = pedidos.rows.length
+    var linha = pedidos.insertRow(qtdLinhas)
+    
+    var cellCod = linha.insertCell(0)
+    var cellProduto = linha.insertCell(1)
+    var cellQtd = linha.insertCell(2)
+    var cellVal = linha.insertCell(3)
+    var cellValT = linha.insertCell(4)
+    
+    cellCod.innerHTML = qtdLinhas;
+    cellProduto.innerHTML = produto
+    cellQtd.innerHTML = qtd
+    cellVal.innerHTML = val
+    cellValT.innerHTML = valt
+}
+
+check.forEach(addEventListener("click", () => {
+    modalFinale.classList.remove('hide')
+    modalFinale.classList.add('finale')
+}))
+
+
+function step(botao){
+    let id = botao.getAttribute("id")
+    let value = quantidade.getAttribute("value")
+    let step = quantidade.getAttribute("step")
+    let calcStep = (id == "dim") ? (step * -1) : (step * 1)
+    let newValue = parseInt(value) + calcStep
+    
+    quantidade.setAttribute("value", newValue)
+}
+
+document.querySelector('[data-button]').addEventListener("click", () =>{
+    let essaQtd = quantidade.getAttribute('value')
+    cart(esseProduto, essaQtd, valor.toFixed(2), (valor*essaQtd).toFixed(2))
+
+    quantidade.setAttribute("value", 0)
+})
+
+function close() {
+    modalFinale.classList.remove('finale')
+    modalFinale.classList.add('hide')
+}
